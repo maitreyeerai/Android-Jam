e89ea34494a06533ab483a9fabce22496fd38771
maitreyeerai <69973701+maitreyeerai@users.noreply.github.com>
2020-12-14 22:00:01 +0530

    Create q1.kt

---
+++ Week-1/q1.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val n=-98.7
+    if (n<0.0)
+        println("$n is a negative number.")
+    else if (n>0.0)
+        println("$n is a positive number.")
+    else
+        println("$n is 0.")
+}
