daf761fc6ca993d3713436409d6f8e577a1e842f
maitreyeerai <69973701+maitreyeerai@users.noreply.github.com>
2020-12-16 00:15:17 +0530

    Update q2.kt

--- Week-1/q2.kt
+++ Week-1/q2.kt
@@ -1,11 +1,8 @@
 fun main() {
-    val a=-9 
-    val b=7 
-    val c=0
-    if (a>=b && a>=c)
-        println("$a is the largest number.")
-    else if (b>=a && b>=c)
-        println("$b is the largest number.")
-    else
-        println("$c is the largest number.")
+    val a=-98
+    val b=77
+    val c=0.9
+    if (a>=b && a>=c) println("$a is the largest number.")
+    else if (b>=a && b>=c) println("$b is the largest number.")
+    else println("$c is the largest number.")
 }
