6ad7d9afcf4b699a123b5699233e3c85a78db17c
maitreyeerai <69973701+maitreyeerai@users.noreply.github.com>
2020-12-15 23:50:10 +0530

    Create q2.kt

---
+++ Week-1/q2.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val a=-9 
+    val b=7 
+    val c=0
+    if (a>=b && a>=c)
+        println("$a is the largest number.")
+    else if (b>=a && b>=c)
+        println("$b is the largest number.")
+    else
+        println("$c is the largest number.")
+}
