ea1aaa29fda1a0b56e631a488d7230b69dfc284a
maitreyeerai <69973701+maitreyeerai@users.noreply.github.com>
2020-12-16 00:13:36 +0530

    Update q1.kt

--- Week-1/q1.kt
+++ Week-1/q1.kt
@@ -1,9 +1,6 @@
 fun main() {
     val n=-98.7
-    if (n<0.0)
-        println("$n is a negative number.")
-    else if (n>0.0)
-        println("$n is a positive number.")
-    else
-        println("$n is 0.")
+    if (n<0.0) println("$n is a negative number.")
+    else if (n>0.0) println("$n is a positive number.")
+    else println("$n is 0.")
 }
